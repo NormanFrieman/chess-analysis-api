bd9a61f0146ce39be16a3a451cc6b7af6c7a494f
Norman Frieman <ezequiel-d.s@hotmail.com>
2021-12-19 13:09:46 -0300

    feat: should return correct values based on validation

--- src/presentation/controllers/load-matches-controller.spec.ts
+++ src/presentation/controllers/load-matches-controller.spec.ts
@@ -1,4 +1,6 @@
-import { IHttpRequest } from "../protocols"
+import { MissingParamError } from "../errors";
+import { badRequest, internalServerError } from "../helpers/http/http-helper";
+import { IHttpRequest, IValidation } from "../protocols"
 import { LoadMatchesController } from "./load-matches-controller"
 
 const makeFakeInput = (): IHttpRequest => {
@@ -10,23 +12,61 @@ const makeFakeInput = (): IHttpRequest => {
     };
 };
 
+const makeValidation = (): IValidation => {
+    class ValidationStub implements IValidation {
+        validate(input: any): Error {
+            return null;
+        }
+    }
+
+    return new ValidationStub();
+}
+
 const makeSut = (): SutTypes => {
+    const validationStub = makeValidation();
+
     return {
-        sut: new LoadMatchesController()
+        sut: new LoadMatchesController(validationStub),
+        validation: validationStub
     }
 }
 
 interface SutTypes {
-    sut: LoadMatchesController
+    sut: LoadMatchesController,
+    validation: IValidation
 }
 
 describe('LoadMatchesController Tests', () => {
-    test('Should return null', async () => {
-        const { sut } = makeSut();
+    test('Should call validation with correct values', async () => {
+        const { sut, validation } = makeSut();
+
+        const validationSpy = jest.spyOn(validation, 'validate');
+
+        const req = makeFakeInput();
+        await sut.handle(req);
+
+        expect(validationSpy).toHaveBeenCalledWith(req.body);
+    }),
+    test('Should return error if validation returns an error', async () => {
+        const { sut, validation } = makeSut();
+
+        jest.spyOn(validation, 'validate').mockReturnValueOnce(new MissingParamError('any error'));
 
         const req = makeFakeInput();
         const res = await sut.handle(req);
 
-        expect(res).toBe(null);
+        expect(res).toEqual(badRequest(new MissingParamError('any error')));
+    }),
+    test('Should return 500 if validation throws', async () => {
+        const { sut, validation } = makeSut();
+
+        jest.spyOn(validation, 'validate').mockImplementationOnce(() => {
+            throw new Error();
+        });
+
+        const req = makeFakeInput();
+        const res = await sut.handle(req);
+
+        expect(res).toEqual(internalServerError(new Error()));
     })
 })
--- src/presentation/controllers/load-matches-controller.ts
+++ src/presentation/controllers/load-matches-controller.ts
@@ -1,7 +1,21 @@
-import { IController, IHttpRequest, IHttpResponse } from "../protocols";
+import { badRequest, internalServerError } from "../helpers/http/http-helper";
+import { IController, IHttpRequest, IHttpResponse, IValidation } from "../protocols";
 
 export class LoadMatchesController implements IController {
+    constructor(
+        private validation: IValidation
+    ) { }
     async handle(httpReq: IHttpRequest): Promise<IHttpResponse> {
-        return new Promise(resolve => resolve(null));
+        try{
+            const validation = this.validation.validate(httpReq.body);
+            if(validation !== null){
+                return badRequest(validation);
+            }
+    
+            return new Promise(resolve => resolve(null));
+        }
+        catch(err){
+            return internalServerError(err);
+        }
     }
 }
